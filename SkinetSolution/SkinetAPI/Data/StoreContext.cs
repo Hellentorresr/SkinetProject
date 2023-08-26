92c544f9894148bee5f37c512e5c118ca7733a9f
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-25 20:22:38 -0600

    Creating the base data

--- SkinetSolution/SkinetAPI/Data/StoreContext.cs
+++ SkinetSolution/SkinetAPI/Data/StoreContext.cs
@@ -1,7 +1,16 @@
-﻿namespace SkinetAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SkinetAPI.Entities;
+
+namespace SkinetAPI.Data
 {
-    public class StoreContext
+    public class StoreContext : DbContext
     {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Products> Products { get; set; }
 
     }
 }
--- SkinetSolution/SkinetAPI/Program.cs
+++ SkinetSolution/SkinetAPI/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SkinetAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer("name=ConnectionToMySQL"));
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
