a16f5c1f33f021b20f8644e6675aa60e3894f8a1
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-29 20:13:27 -0600

    Adding SeedData folder and three files to with data as json files

---
+++ SkinetSolution/Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,49 @@
+﻿using Core.Entities;
+using System.Text.Json;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            //Brands table
+            if (!context.Brands.Any())
+            {
+                var brandData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+
+                //storing the data into the database
+                context.Brands.AddRange(brands);
+            }
+
+
+            //ProductType Table
+            if (!context.ProductTypes.Any())
+            {
+                var typeData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                var producTypes = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+
+                //storing the data into the database
+                context.ProductTypes.AddRange(producTypes);
+            }
+
+            //Product Table
+            if (!context.Products.Any())
+            {
+                var productData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+
+                var products = JsonSerializer.Deserialize<List<Products>>(productData);
+
+                //storing the data into the database
+                context.Products.AddRange(products);
+            }
+
+
+
+            if(context.ChangeTracker.HasChanges()) await context.SaveChangesAsync();
+        }
+    }
+}
