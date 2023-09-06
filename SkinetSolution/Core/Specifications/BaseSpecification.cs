0e42c1a2d030d94c1c47ba3c5fa36a31588a9f54
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-09-05 20:17:05 -0600

    Adding class BaseSpecification

---
+++ SkinetSolution/Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        // >> Properties << 
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } =
+            new List<Expression<Func<T, object>>>();
+
+
+        //  >> Constructors <<
+        public BaseSpecification(){}
+
+        public BaseSpecification(Expression<Func<T, bool>> criteria) { 
+            Criteria = criteria;
+        }
+
+        //creating a method to add include statements to the Include list
+        protected void AddInclude(Expression<Func<T, object>> includeExpression) 
+        { 
+            //This property `Include`
+            Includes.Add(includeExpression);
+        }
+    }
+}
