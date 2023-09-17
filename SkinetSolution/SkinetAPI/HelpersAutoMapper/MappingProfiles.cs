d45d7132596fd90eb79e4eef05df39417bd60b26
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-09-16 19:42:45 -0600

    Adding mapping

--- SkinetSolution/SkinetAPI/HelpersAutoMapper/MappingProfiles.cs
+++ SkinetSolution/SkinetAPI/HelpersAutoMapper/MappingProfiles.cs
@@ -8,7 +8,10 @@ namespace SkinetAPI.HelpersAutoMapper
     {
         public MappingProfiles() 
         { 
-            CreateMap<Products, ProductToReturnDTO>();
+            CreateMap<Products, ProductToReturnDTO>().
+                ForMember(dto => dto.ProductBrand, opc => opc.MapFrom(source => source.ProductBrand.Name)).
+                ForMember(dto => dto.ProductType, opc => opc.MapFrom(source => source.ProductType.Name)).
+                ForMember(d => d.PictureUrl, o => o.MapFrom<ProductImgUrlResolver>());
         }
     }
 }
